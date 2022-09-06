c025aa407b2ca6c07a158175855c203dcd233f66
CYY1007 <younguk86@gmail.com>
2022-09-07 08:51:14 +0900

    Create Account

---
+++ users/users.mutations.js
@@ -0,0 +1,31 @@
+import client from "../client"
+import bcrypt from "bcrypt"
+
+export default {
+    Mutation:{
+        createAccount : async (_,{firstName,
+            lastName,
+            username,
+            email,
+            password}) => {
+                try{
+                    const existingUser = await client.user.findFirst({
+                        where: {
+                            OR:[
+                                {username},{email}
+                            ]
+                        },
+                    })
+                    if (existingUser){
+                        throw new Error("This username/emial is already taken")
+                    }
+                    const hashedPassword =await bcrypt.hash(password,8);
+                    return client.user.create({data:{
+                        username,email,firstName,lastName,password:hashedPassword
+                    }})
+                }catch(e){
+                    return e;
+                }
+            }
+    }
+}  
